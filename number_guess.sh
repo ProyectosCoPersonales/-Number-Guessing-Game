87d6bf80ef5c3514b8c026a5146646e209356243
JordanDeev Llumitaxi <prograllumitaxi@outlook.com>
2024-11-03 05:24:54 +0000

    feat: create function to check the guess number

--- number_guess.sh
+++ number_guess.sh
@@ -61,3 +61,44 @@ INPUT_GUESS() {
     CHECK_ANSWER $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
   fi
 }
+CHECK_ANSWER() {
+  USER_NAME=$1 
+  CORRECT_ANSWER=$2 
+  GUESS_COUNT=$3
+  USSER_GUESS=$4
+  
+  #Until they guess the secret number, 
+  #it should print 
+  #It's lower than that, guess again: if the previous input was higher than the secret number, and
+  #It's higher than that, guess again: if the previous input was lower than the secret number. 
+  #Asking for input each time until they input the secret number.
+  if [[ $USSER_GUESS -lt $CORRECT_ANSWER ]]
+  then
+    echo "It's lower than that, guess again:"
+    read USSER_GUESS
+  elif [[ $USSER_GUESS -gt $CORRECT_ANSWER ]]
+  then
+    echo "It's higher than that, guess again:"
+    read USSER_GUESS
+  else
+    GUESS_COUNT=$GUESS_COUNT
+  fi
+
+  GUESS_COUNT=$(( $GUESS_COUNT + 1 ))
+  if [[ ! $USSER_GUESS =~ ^[0-9]+$ ]]
+  then
+    INPUT_GUESS $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
+  elif [[ $USSER_GUESS -lt $CORRECT_ANSWER ]] || [[ $USSER_GUESS -gt $CORRECT_ANSWER ]]
+  then
+    CHECK_ANSWER $USER_NAME $CORRECT_ANSWER $GUESS_COUNT $USSER_GUESS
+  elif [[ $USSER_GUESS -eq $CORRECT_ANSWER ]]
+  then
+    #When the secret number is guessed, your script should print You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. 
+    #Nice job! and finish running
+    SAVE_USER $USER_NAME $GUESS_COUNT
+    NUMBER_OF_GUESSES=$GUESS_COUNT
+    SECRET_NUMBER=$CORRECT_ANSWER
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+  fi
+
+}
